ac7d7b4a3cde8ec993908e78231da67803f68099
Chris Jewell <chrisjohnjewell@gmail.com>
2016-10-24 20:49:16 +1100

    Try and travel through time when we run the gem

--- lib/git_time_machine.rb
+++ lib/git_time_machine.rb
@@ -1,9 +1,26 @@
 require "git_time_machine/version"
+require "git_time_machine/time_machine"
 
 module GitTimeMachine
   module TravelThroughTime
+    class LawsOfPhysicsError < RuntimeError; end
+
     def self.travel_through_time
-      raise NotImplementedError, "TODO"
+      time_machine = TimeMachine.new
+      time_machine.get_ready!
+
+      if can_travel_through_time?(time_machine)
+        time_machine.back_to "1955"
+      else
+        raise LawsOfPhysicsError, 'Cannot Travel Through Time'
+      end
+    end
+
+    private
+
+    def self.can_travel_through_time?(time_machine)
+      time_machine.flux_capacitated? &&
+        (time_machine.velocity >= 88)
     end
   end
 end
